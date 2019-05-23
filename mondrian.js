3cf334cc5a17def9c217fef6a05cf53c0525be7a
Tom <thomas.lee@uconn.edu>
2019-05-23 07:44:58 -0400

    basic square grid with primary colors

---
+++ mondrian.js
@@ -0,0 +1,41 @@
+
+function setup() {
+  w = 2001
+  h = 1001
+  createCanvas(w,h)
+  unit = 50
+  strokeWeight(8)
+  rect(h/2,w/2,unit,unit)
+  for(var i = 0; i < w; i+=unit){
+    for(var j = 0; j < h; j+=unit){
+      fill(randomMondrianColor())
+      rect(i,j,unit,unit)
+    }   
+  }
+}
+
+function randomMondrianColor(){
+  //var red = color(255,0,0)
+  //var blue = color(0,0,255)
+  //var yellow = color(255,255,0)
+  //var white = color(255,255,255)
+  //var black = color(0,0,0)
+
+  var p = random(1)
+  if(p < 0.8){
+    return color(255,255,255)
+  }
+  else{
+    if(p < 0.85){
+      return color(255,255,0)
+    }
+    if(p < 0.9){
+      return color(0,0,255)
+    }
+    else return color(255,0,0)
+  }
+}
+
+function draw() {
+  
+}
