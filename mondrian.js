c227854f0917ba2ea54d1c7da9d8e132016ca801
Tom <thomas.lee@uconn.edu>
2019-05-24 07:30:55 -0400

    white to colored block ratio slider

--- mondrian.js
+++ mondrian.js
@@ -1,14 +1,37 @@
+var whiteRatioSlider
+var whiteRatioVal
+var gridscale, unit
+var frameW, frameH
 
 function setup() {
-  w = 2001
+  w = 3000
   h = 1001
-  gridscale = 1
+  frameW = 2001
+  frameH = 1001
+  
+  gridscale = .5
   createCanvas(w,h)
   unit = 50 * gridscale
-  strokeWeight(unit/4)
-  rect(h/2,w/2,unit,unit)
-  for(var i = 0; i < w; i+=unit){
-    for(var j = 0; j < h; j+=unit){
+  
+  strokeWeight(unit/5)
+  
+  whiteRatioSlider = createSlider(10,90,1);
+  whiteRatioSlider.position(2050,10);
+  whiteRatioSlider.style('width', '80px');
+  whiteRatioVal = whiteRatioSlider.value()
+  drawGrid(frameW,frameH,unit)
+}
+
+function draw() {
+  if(whiteRatioVal != whiteRatioSlider.value()){
+    whiteRatioVal = whiteRatioSlider.value()
+    drawGrid(frameW,frameH,unit)
+  }
+}
+
+function drawGrid(x,y,gridUnit){
+    for(var i = 0; i < frameW; i+=gridUnit){
+    for(var j = 0; j < frameH; j+=gridUnit){
       drawBlock(i,j,unit)
       //drawBlockRandomSize(i,j,unit)
       //drawBlockRandomPosition(i,j,unit)
@@ -44,27 +67,17 @@ function drawBlockRandomPositionAndSize(x,y,size){
 }
 
 function randomMondrianColor(){
-  //var red = color(255,0,0)
-  //var blue = color(0,0,255)
-  //var yellow = color(255,255,0)
+  var red = color(255,0,0)
+  var blue = color(0,0,255)
+  var yellow = color(255,255,0)
   //var white = color(255,255,255)
   //var black = color(0,0,0)
-
-  var p = random(1)
-  if(p < 0.8){
+  var colors = [red,blue,yellow]
+  var p = random(10,90)
+  if(p < whiteRatioVal){
     return color(255,255,255)
   }
   else{
-    if(p < 0.85){
-      return color(255,255,0)
-    }
-    if(p < 0.9){
-      return color(0,0,255)
-    }
-    else return color(255,0,0)
+    return random(colors)
   }
-}
-
-function draw() {
-  
 }
